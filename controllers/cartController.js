f1d6c12f312ce772297410df1044e870991ee533
kunal kumar <kunalkumar17aug@gmail.com>
2025-05-20 00:28:53 +0530

    updated many things

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -5,7 +5,7 @@ module.exports.addToCart = async (req, res) => {
     const token = req.cookies.jwt;
 
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+        
     }
 
     try {
